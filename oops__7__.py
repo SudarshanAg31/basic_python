b333b1b98001ee3a6ab697625c341ce4e939f8c3
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-03-19 23:37:39 +0530

    learn abstraction & Encapsulation meaning and definition

---
+++ oops__7__.py
@@ -0,0 +1,6 @@
+#abstraction:-
+#hiding implementation details of a class and
+#only showing the essential features to the usser.
+
+#Encapsulation:-
+#Wrapping data and function into a single unit(objects).
