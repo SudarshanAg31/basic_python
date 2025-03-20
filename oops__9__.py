14f4252a4a3932b4e051860f034e80777ea9d41e
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-03-20 23:03:09 +0530

    learn aobut delete keyword

---
+++ oops__9__.py
@@ -0,0 +1,12 @@
+#del keyword: used to delete object properties or object itself.
+class student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+s1=student("Sudarshan Agrawal",19)
+del s1.age
+print(s1.name)
+del s1
+print(s1.age,s1.name)
+#NOTE: if we use (del s1) so it delete whole s1 (object)
+#NOTE: if we use(del s1.name) so it delete name in s1 (object properties)
