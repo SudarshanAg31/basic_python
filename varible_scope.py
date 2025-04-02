2f8087c505ce8270715f770f001989fa471c141d
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-04-02 10:58:59 +0530

    add global function

--- varible_scope.py
+++ varible_scope.py
@@ -9,12 +9,15 @@ def func1():
 func1()
 # here x=4 is local varible and x=3 is enclosed
 def func1():
-    print(x)
-def func2():
-    print(x)
-x=13
-func2()
-func1()
+    global x
+    a=5
+    a=a+x
+    x=a
+    return x     
+x=15
+print(func1())
+# if we not use the global so it show error because we can't change the value over the function
+#global varible is use to change the value over the function.
 #here x=13 is global varible
 from math import e
 def func1():
