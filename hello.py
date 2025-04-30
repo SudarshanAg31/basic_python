9c9c4111d1236cf1380d4dfe61681833c876b76f
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2025-04-30 21:38:44 +0530

    rest for paper

--- hello.py
+++ hello.py
@@ -17,4 +17,5 @@ print("rest for paper")
 print("rest for paper")
 print("rest for paper")
 print("rest for paper")
+print("rest for paper")
 
--- try.py
+++ try.py
@@ -1,8 +1,2 @@
-n=int(input())
-s=[]
-for i in range(0,n):
-    x=input()
-    s.append(x) 
-s=set(s)
-s=len(s)
-print(s)
+a=2e4
+print(a)
